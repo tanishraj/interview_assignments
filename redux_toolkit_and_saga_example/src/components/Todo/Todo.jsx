99a93789c2e48f5bcfc5d641bd8312fa939c6fd1
tanishraj <learn.tanishraj@gmail.com>
2024-05-24 01:35:16 +0530

    todo component updated

--- redux_toolkit_and_saga_example/src/components/Todo/Todo.jsx
+++ redux_toolkit_and_saga_example/src/components/Todo/Todo.jsx
@@ -1,32 +1,37 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { deleteTodoRequest } from "../../store/slices/todoSlice";
 import "./Todo.css";
 
 export const Todo = () => {
   const todoList = useSelector((state) => {
-    return state.todos;
+    return state.todos.todo;
   });
-  const { data, isLoading, errors } = todoList?.todos;
+  const { data, isLoading, errors } = todoList;
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch({ type: "FETCH_TODOS" });
-  }, []);
-
-  console.log("TODOS RESULT", todoList);
+  }, [dispatch]);
 
   return (
     <div className="wrapper">
       <div className="container">
-        <h4>Task List</h4>
+        <h3>Task List</h3>
         {errors && <p>{errors}</p>}
         {isLoading ? (
           <p>Loading...</p>
         ) : (
           <ul className="task-list">
-            {data?.todos?.map((todo) => (
+            {data?.map((todo) => (
               <li key={todo.id} className="task">
-                {todo.todo}
+                <span>{todo.todo}</span>
+                <span className="button-container">
+                  <button onClick={() => {}}>Edit</button>
+                  <button onClick={() => dispatch(deleteTodoRequest(todo.id))}>
+                    Delete
+                  </button>
+                </span>
               </li>
             ))}
           </ul>
